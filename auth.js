c321a805b8153e14b39f9a0b6be9687841814410
MinJun Kweon <Minz0000000@gmail.com>
2018-12-01 21:05:38 +0900

    apply to auth

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 module.exports = function (req, res, next) {
-    // if (!req.session.user) {
-    //     return res.status(403).json({ 'code': 403, 'message' : 'forbidden'});
-    // }
+    if (!req.session.user) {
+        return res.status(403).json({ 'code': 403, 'message' : 'forbidden'});
+    }
     return next();
 }
